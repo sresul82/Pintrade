04cda0cfec72292181fe50e327d3a645e00a2962
sresul82 <s.resul82@gmail.com>
2026-05-17 22:14:01 +0500

    trade angle fix

--- js/drawing/ui/drawing-settings-dialog.js
+++ js/drawing/ui/drawing-settings-dialog.js
@@ -81,7 +81,7 @@ const DrawingSettingsDialog = (() => {
     ray:          { priceLabel:true,  extend:true,  midpoint:true,  stats:true,  capArrows:true,  hasFill:false, hasText:true, coordsMode:'p2' },
     extended:     { priceLabel:true,  extend:true,  midpoint:true,  stats:true,  capArrows:true,  hasFill:false, hasText:true, coordsMode:'p2' },
     infoline:     { priceLabel:true,  extend:true,  midpoint:true,  stats:true,  capArrows:false, hasFill:false, coordsMode:'p2'       },
-    trendangle:   { priceLabel:true,  extend:true,  midpoint:false, stats:false, capArrows:false, hasFill:false, hasText:false, coordsMode:'p2' },
+    trendangle:   { priceLabel:true,  extend:true,  midpoint:true,  stats:true,  capArrows:true,  hasFill:false, hasText:false, coordsMode:'p2' },
     arrowdraw:    { priceLabel:false, extend:false, midpoint:false, stats:false, capArrows:true,  hasFill:false, coordsMode:'p2'       },
     channel:      { priceLabel:false, extend:true,  midpoint:false, stats:false, capArrows:false, hasFill:true,  coordsMode:'p2'       },
     regression:   { priceLabel:false, extend:false, midpoint:false, stats:false, capArrows:false, hasFill:false, coordsMode:'p2'       },
